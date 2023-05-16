ff54d2c88c01733129b569e6b815cafd084ea983
Nick Barry <nick.barry@additiveassurance.com>
2023-05-16 11:45:42 +1000

    wip: mastodon require input

--- mastodon_harvester/main.py
+++ mastodon_harvester/main.py
@@ -1,6 +1,14 @@
 import harvester
+import sys
+import requests
+import subprocess
 
 if __name__ == "__main__":
     print("Running main")
     harvester.harvest()
     print("Imported from /harvester")
+    if sys.argv[-1] == "post":
+        print("posting")
+        command = 'curl -X PUT "http://admin:admin@172.26.135.12:5984/twitter/brad" --header "Content-Type:application/json" --data \'{"type": "account", "holder": "Charlie","initialbalance": 100}\''
+        subprocess.run(command, shell=True)
+        print("posted")
