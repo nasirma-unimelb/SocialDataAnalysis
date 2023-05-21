83ea4b0e9606342938b78ce0e43552a4635b0fd0
Nick Barry <nick.barry@additiveassurance.com>
2023-05-21 21:19:29 +1000

    fix: typo MASTODON_KEY

--- mastodon_harvester/entrypoint.sh
+++ mastodon_harvester/entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo ${ADO_API_KEY}
 
-python ./mastodon_harvester.py --ado_api_key ${ADO_API_KEY} --ado_login_url ${ADO_LOGIN} --ado_text_search_mastodon_url ${ADO_SEARCH} --mastodon_api_key ${MASOTDON_KEY} --couchdb_ip ${COUCHDB_IP} --couchdb_user ${COUCHDB_USER} --couchdb_password ${COUCHDB_PASSWORD}
+python ./mastodon_harvester.py --ado_api_key ${ADO_API_KEY} --ado_login_url ${ADO_LOGIN} --ado_text_search_mastodon_url ${ADO_SEARCH} --mastodon_api_key ${MASTODON_KEY} --couchdb_ip ${COUCHDB_IP} --couchdb_user ${COUCHDB_USER} --couchdb_password ${COUCHDB_PASSWORD}
