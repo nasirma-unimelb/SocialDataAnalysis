29f6fa3cf23100f5c34143c6f916091e3343872a
Nick Barry <nick.barry@additiveassurance.com>
2023-05-21 20:55:12 +1000

    wip: mastodon dockerupdates

--- mastodon_harvester/entrypoint.sh
+++ mastodon_harvester/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-python ./mastodon_harvester.py --ado_api_key ${ADO_API_KEY} --ado_login_url ${ADO_LOGIN} --ado_text_search_mastodon_url ${ADO_SEARCH} --mastodon_api_key ${MASOTDON_KEY} --couchdb_ip ${COUCHDB_IP}
+python ./mastodon_harvester.py --ado_api_key ${ADO_API_KEY} --ado_login_url ${ADO_LOGIN} --ado_text_search_mastodon_url ${ADO_SEARCH} --mastodon_api_key ${MASOTDON_KEY} --couchdb_ip ${COUCHDB_IP} --couchdb_user ${COUCHDB_USER} --couchdb_password ${COUCHDB_PASSWORD}
